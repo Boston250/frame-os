cfd9492ddfb6a7f751d772b99eb1416702860900
Boston250 <niyigenajosue9@qq.com>
2026-09-14 17:19:06 +0800

    Clear reporting links before employee reset

--- database/026-reset-operational-records-preserve-frm-0003.sql
+++ database/026-reset-operational-records-preserve-frm-0003.sql
@@ -13,5 +13,6 @@ END $$;
 DELETE FROM employee_roles WHERE employee_id NOT IN (SELECT id FROM employees WHERE employee_number='FRM-0003');
 DELETE FROM user_accounts WHERE employee_id NOT IN (SELECT id FROM employees WHERE employee_number='FRM-0003');
 ALTER TABLE employees DISABLE TRIGGER employees_no_delete;
+UPDATE employees SET manager_id=NULL;
 DELETE FROM employees WHERE employee_number<>'FRM-0003';
 ALTER TABLE employees ENABLE TRIGGER employees_no_delete;
